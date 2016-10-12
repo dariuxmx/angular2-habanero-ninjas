1cc551c5f7a99b23bf2d6a8106c1158baf6adb6e
Darío Gutiérrez <dario@habanerostudio.mx>
2016-10-12 09:21:28 -0500

    Set up project, playing with some binding data and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/index';
 
 @Component({
+  //Root component
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  /*
+  Child components
+  Add header component*/
+  directives: [HomeComponent]
 })
 export class AppComponent {
   title = 'app works!';
+  name = 'Habanero Ninja';
 }
